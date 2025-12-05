bd39fc5c653e09293d89f8590a874d3e5601f929
f1d02310006adelia <baiqdelia01@gmail.com>
2025-12-05 17:37:58 +0800

    finish commit

--- DaoMarker.java
+++
@@ -1,7 +0,0 @@
-/**
- * Marker interface for DAO classes. Berguna untuk menunjukkan bahwa kelas terkait
- * adalah objek akses data dan dapat digunakan untuk penandaan tipe/DI pada latihan PBO.
- */
-public interface DaoMarker {
-
-}
